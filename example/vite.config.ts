61f6d769004ad4d9c2bde3a12e928b487f54dd0e
jinsewoon-dev <jinsewoon.dev@gmail.com>
2024-12-17 22:47:05 +0900

    feat : tailwind 초기 설정

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@shadcn": path.resolve(__dirname, "./src/shadcn"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@templates": path.resolve(__dirname, "./src/templates"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@stores": path.resolve(__dirname, "./src/stores"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+    },
+  },
+});
