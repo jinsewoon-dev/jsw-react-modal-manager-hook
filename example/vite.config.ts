5e03e19f24f0acf933336cb7fd228f7fe6766d2f
jinsewoon-dev <jinsewoon.dev@gmail.com>
2024-12-17 17:46:48 +0900

    feat : shadcn cli  설치 및 폴더 경로 설정

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@shadcn": path.resolve(__dirname, "./src/shadcn"),
     },
   },
 });
