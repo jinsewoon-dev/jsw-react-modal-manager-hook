c9ca9338d42d3a821ba7dcddb5d21406033111bc
jinsewoon-dev <jinsewoon.dev@gmail.com>
2024-12-17 22:12:37 +0900

    chore: vite.config.js에 alias 추가 설정

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -8,6 +8,14 @@ export default defineConfig({
   resolve: {
     alias: {
       "@shadcn": path.resolve(__dirname, "./src/shadcn"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@stores": path.resolve(__dirname, "./src/stores"),
+      "@templates": path.resolve(__dirname, "./src/templates"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
     },
   },
 });
